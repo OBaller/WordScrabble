c10ae17aa96b56bade7406b296d1b267a9e4be1a
brightsummit.co <naseemoyebola@gmail.com>
2022-01-26 01:25:05 +0100

    Initial Commit

---
+++ WordScrabble/WordScrabble/WordScrabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrabbleApp.swift
+//  WordScrabble
+//
+//  Created by naseem on 26/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
